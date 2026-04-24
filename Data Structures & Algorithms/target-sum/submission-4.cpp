c27ec9819319a0cf978eef5c2df614b2b12eb640
AmeenRizvi <120264332+AmeenRizvi@users.noreply.github.com>
2026-04-24 16:08:55 +0530

    Add: target-sum - submission-4

---
+++ Data Structures & Algorithms/target-sum/submission-4.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<unordered_map<int, int>>dp(n+1);
+        dp[n][0] = 1;
+
+        for(int i = n-1; i >= 0; i--)
+        {
+            for(auto &p : dp[i+1])
+            {
+                dp[i][p.first + nums[i]] += p.second;
+                dp[i][p.first - nums[i]] += p.second;
+            }
+        }
+
+        return dp[0][target];
+        
+    }
+};
