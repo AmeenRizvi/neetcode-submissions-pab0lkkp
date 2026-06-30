9bbe2b0997d470e5d27a4ea74f579e2ebcd07ab3
AmeenRizvi <120264332+AmeenRizvi@users.noreply.github.com>
2026-06-30 15:34:39 +0530

    Add: find-lucky-integer-in-an-array - submission-0

---
+++ Data Structures & Algorithms/find-lucky-integer-in-an-array/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        int n = arr.size();
+        unordered_map<int,int>freq;
+
+        for(int i = 0; i < n; i++)
+        {
+            freq[ arr[i] ]++;
+        }
+
+        int ans = -1;
+        for(auto &[num, count] : freq)
+        {
+            if(num == count)
+            {
+                ans = max(ans, num);
+            }
+
+        }
+        return ans;
+        
+    }
+};
