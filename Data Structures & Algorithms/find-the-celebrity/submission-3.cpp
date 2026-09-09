e3f9946642fddb5f4d0a8fb4c594dce5005644db
AmeenRizvi <120264332+AmeenRizvi@users.noreply.github.com>
2026-09-09 11:15:10 +0530

    Add: find-the-celebrity - submission-3

---
+++ Data Structures & Algorithms/find-the-celebrity/submission-3.cpp
@@ -0,0 +1,31 @@
+/* The knows API is defined for you.
+      bool knows(int a, int b); */
+
+class Solution {
+public:
+    int findCelebrity(int n) {
+        vector<int>indegree(n), outdegree(n);
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(i != j && knows(i, j) )
+                {
+                    indegree[j]++;
+                    outdegree[i]++;
+                }
+            }
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            if(indegree[i] == n-1 && outdegree[i] == 0)
+            {
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+};
