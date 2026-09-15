0fb455a6c1e65a7af2367be07a85ce8db22fbd44
AmeenRizvi <120264332+AmeenRizvi@users.noreply.github.com>
2026-09-15 20:33:38 +0530

    Add: power-of-two - submission-1

---
+++ Data Structures & Algorithms/power-of-two/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n <= 0)
+        {
+            return false;
+        }
+
+        while(n > 1)
+        {
+            int r = n % 2;
+            n = n / 2;
+            if(r != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
