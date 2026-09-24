6876ca2dd808c6da8555d40bd6445f139fce1dab
AmeenRizvi <120264332+AmeenRizvi@users.noreply.github.com>
2026-09-24 23:19:09 +0530

    Add: valid-word-abbreviation - submission-1

---
+++ Data Structures & Algorithms/valid-word-abbreviation/submission-1.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool validWordAbbreviation(string word, string abbr) {
+        int n = word.size(), m = abbr.size();
+        int i = 0, j = 0;
+
+        while(j < m)
+        {
+            if(isalpha(abbr[j] ) )
+            {
+                if(i >= n || word[i] != abbr[j])
+                {
+                    return false;
+                }
+                i++;
+                j++;
+            }
+            else
+            {
+                int num = 0;
+                if(abbr[j] == '0') return false;
+
+                while(j < m && !isalpha(abbr[j]) )
+                {
+                    num = num*10 + (abbr[j]-'0');
+                    j++;
+                }
+                if(i + num > n)
+                {
+                    return false;
+                }
+                i = i + num;
+            }
+        }
+
+        return i == n;
+        
+    }
+};
